e18789d7f3911882b9ac574b61d482dbfed5deda
NicolasAraya932 <nicoarayapcap@email.com>
2023-08-13 20:35:44 -0400

    SERVER MADE WITH OXMYSQL AND VRP FOR GTA WORKING

--- server/server-data/resources/[vrp]/vrp/cfg/base.lua
+++ server/server-data/resources/[vrp]/vrp/cfg/base.lua
@@ -7,7 +7,7 @@ cfg.db = {
   host = "127.0.0.1",
   database = "vRP",
   user = "vRP",
-  password = ""
+  password = "password"
 }
 
 cfg.server_id = "main" -- identify the server (ex: in database)
--- server/server-data/resources/[vrp]/vrp_oxmysql/init.lua
+++ server/server-data/resources/[vrp]/vrp_oxmysql/init.lua
@@ -1,5 +1,5 @@
 local Proxy = module("vrp", "lib/Proxy")
-local vRP = Proxy.getInterface("vrp")
+local vRP = Proxy.getInterface("vRP")
 
 async(function()
   vRP.loadScript("vrp_oxmysql", "init_vrp")
